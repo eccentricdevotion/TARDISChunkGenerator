b18bcbe3f04422be5852baad7a53149533d8f34b
Eccentric Devotion <eccentricdevotion@gmail.com>
2021-11-27 16:40:41 +1300

    Biomes no longer have depth and scale + new / removed biome names

--- src/main/java/me/eccentric_nz/tardischunkgenerator/custombiome/CustomBiomeData.java
+++ src/main/java/me/eccentric_nz/tardischunkgenerator/custombiome/CustomBiomeData.java
@@ -4,28 +4,19 @@ public class CustomBiomeData {
 
     private final String minecraftName;
     private final String customName;
-    private float depth;
-    private float scale;
-    private float temperature;
-    private float downfall;
-    private int fogColour;
-    private int waterColour;
-    private int waterFogColour;
-    private int skyColour;
-    private int foliageColour;
-    private int grassColour;
-    private boolean frozen;
+    private final float temperature;
+    private final float downfall;
+    private final int fogColour;
+    private final int waterColour;
+    private final int waterFogColour;
+    private final int skyColour;
+    private final int foliageColour;
+    private final int grassColour;
+    private final boolean frozen;
 
-    public CustomBiomeData(String minecraftName, String customName) {
+    public CustomBiomeData(String minecraftName, String customName, float temperature, float downfall, int fogColour, int waterColour, int waterFogColour, int skyColour, int foliageColour, int grassColour, boolean frozen) {
         this.minecraftName = minecraftName;
         this.customName = customName;
-    }
-
-    public CustomBiomeData(String minecraftName, String customName, float depth, float scale, float temperature, float downfall, int fogColour, int waterColour, int waterFogColour, int skyColour, int foliageColour, int grassColour, boolean frozen) {
-        this.minecraftName = minecraftName;
-        this.customName = customName;
-        this.depth = depth;
-        this.scale = scale;
         this.temperature = temperature;
         this.downfall = downfall;
         this.fogColour = fogColour;
@@ -49,91 +40,39 @@ public class CustomBiomeData {
         return customName;
     }
 
-    public float getDepth() {
-        return depth;
-    }
-
-    public void setDepth(float depth) {
-        this.depth = depth;
-    }
-
-    public float getScale() {
-        return scale;
-    }
-
-    public void setScale(float scale) {
-        this.scale = scale;
-    }
-
     public float getTemperature() {
         return temperature;
-    }
-
-    public void setTemperature(float temperature) {
-        this.temperature = temperature;
     }
 
     public float getDownfall() {
         return downfall;
     }
 
-    public void setDownfall(float downfall) {
-        this.downfall = downfall;
-    }
-
     public int getFogColour() {
         return fogColour;
-    }
-
-    public void setFogColour(String fogColour) {
-        this.fogColour = fromHex(fogColour);
     }
 
     public int getWaterColour() {
         return waterColour;
     }
 
-    public void setWaterColour(String waterColour) {
-        this.waterColour = fromHex(waterColour);
-    }
-
     public int getWaterFogColour() {
         return waterFogColour;
-    }
-
-    public void setWaterFogColour(String waterFogColour) {
-        this.waterFogColour = fromHex(waterFogColour);
     }
 
     public int getSkyColour() {
         return skyColour;
     }
 
-    public void setSkyColour(String skyColour) {
-        this.skyColour = fromHex(skyColour);
-    }
-
     public int getFoliageColour() {
         return foliageColour;
-    }
-
-    public void setFoliageColour(String foliageColour) {
-        this.foliageColour = fromHex(foliageColour);
     }
 
     public int getGrassColour() {
         return grassColour;
     }
 
-    public void setGrassColour(String grassColour) {
-        this.grassColour = fromHex(grassColour);
-    }
-
     public boolean isFrozen() {
         return frozen;
-    }
-
-    public void setFrozen(boolean frozen) {
-        this.frozen = frozen;
     }
 }
--- src/main/java/me/eccentric_nz/tardischunkgenerator/custombiome/TARDISBiomeData.java
+++ src/main/java/me/eccentric_nz/tardischunkgenerator/custombiome/TARDISBiomeData.java
@@ -5,8 +5,6 @@ public class TARDISBiomeData {
     public static final CustomBiomeData BADLANDS = new CustomBiomeData(
             "badlands",
             "gallifrey_badlands",
-            0.1f,
-            0.2f,
             2.0f,
             0,
             16771522,
@@ -21,8 +19,6 @@ public class TARDISBiomeData {
     public static final CustomBiomeData ERODED = new CustomBiomeData(
             "eroded_badlands",
             "gallifrey_eroded",
-            0.1f,
-            0.2f,
             2.0f,
             0,
             16771522,
@@ -35,10 +31,8 @@ public class TARDISBiomeData {
     );
 
     public static final CustomBiomeData PLATEAU = new CustomBiomeData(
-            "badlands_plateau",
+            "wooded_badlands",
             "gallifrey_plateau",
-            1.5f,
-            0.025f,
             2.0f,
             0,
             16771522,
@@ -53,8 +47,6 @@ public class TARDISBiomeData {
     public static final CustomBiomeData DESERT = new CustomBiomeData(
             "desert",
             "skaro_desert",
-            0.125f,
-            0.05f,
             2.0f,
             0,
             13615871,
@@ -67,10 +59,8 @@ public class TARDISBiomeData {
     );
 
     public static final CustomBiomeData HILLS = new CustomBiomeData(
-            "desert_hills",
+            "desert",
             "skaro_hills",
-            0.45f,
-            0.3f,
             2.0f,
             0,
             13615871,
@@ -83,10 +73,8 @@ public class TARDISBiomeData {
     );
 
     public static final CustomBiomeData LAKES = new CustomBiomeData(
-            "desert_lakes",
+            "desert",
             "skaro_lakes",
-            0.225f,
-            0.25f,
             2.0f,
             0,
             13615871,
