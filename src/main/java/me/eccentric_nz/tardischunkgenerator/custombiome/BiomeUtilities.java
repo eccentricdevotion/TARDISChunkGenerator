62ec960f8f7e09dd1d8491aea2a60957fc4f7782
Eccentric Devotion <eccentricdevotion@gmail.com>
2021-11-29 18:46:14 +1300

    Reduce & remove some custom biome code

--- src/main/java/me/eccentric_nz/tardischunkgenerator/custombiome/BiomeUtilities.java
+++ src/main/java/me/eccentric_nz/tardischunkgenerator/custombiome/BiomeUtilities.java
@@ -4,20 +4,14 @@ import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.dedicated.DedicatedServer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.biome.Biome;
-import net.minecraft.world.level.biome.BiomeSpecialEffects;
-import net.minecraft.world.level.chunk.LevelChunk;
 import net.minecraft.world.phys.Vec3;
 import org.bukkit.Bukkit;
-import org.bukkit.Chunk;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.craftbukkit.v1_18_R1.CraftChunk;
-import org.bukkit.craftbukkit.v1_18_R1.CraftServer;
 import org.bukkit.craftbukkit.v1_18_R1.CraftWorld;
 import org.bukkit.craftbukkit.v1_18_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
@@ -28,7 +22,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Optional;
 import java.util.Properties;
-import java.util.logging.Level;
 
 public class BiomeUtilities {
 
@@ -51,14 +44,10 @@ public class BiomeUtilities {
         if (planets.getBoolean("planets." + levelName + "_tardis_gallifrey.enabled")) {
             Bukkit.getConsoleSender().sendMessage(messagePrefix + "Adding custom biomes for planet Gallifrey...");
             CustomBiome.addCustomBiome(TARDISBiomeData.BADLANDS);
-            CustomBiome.addCustomBiome(TARDISBiomeData.ERODED);
-            CustomBiome.addCustomBiome(TARDISBiomeData.PLATEAU);
         }
         if (planets.getBoolean("planets." + levelName + "_tardis_skaro.enabled")) {
             Bukkit.getConsoleSender().sendMessage(messagePrefix + "Adding custom biomes for planet Skaro...");
             CustomBiome.addCustomBiome(TARDISBiomeData.DESERT);
-            CustomBiome.addCustomBiome(TARDISBiomeData.HILLS);
-            CustomBiome.addCustomBiome(TARDISBiomeData.LAKES);
         }
     }
 
@@ -77,75 +66,5 @@ public class BiomeUtilities {
             }
         }
         return null;
-    }
-
-    public static String getBiomeKey(Location location) {
-        CraftWorld world = (CraftWorld) location.getWorld();
-        ServerLevel worldServer = world.getHandle();
-        Registry<Biome> registry = worldServer.registryAccess().registry(Registry.BIOME_REGISTRY).get();
-        LevelChunk chunk = ((CraftChunk) location.getChunk()).getHandle();
-        ResourceLocation key;
-        Biome base = chunk.getNoiseBiome(8, 64, 8);
-        BiomeSpecialEffects fog = base.getSpecialEffects();
-        if (fog.getSkyColor() == TARDISBiomeData.LAKES.getSkyColour()) {
-            key = new ResourceLocation("tardis", "skaro_lakes");
-        } else if (fog.getSkyColor() == TARDISBiomeData.HILLS.getSkyColour()) {
-            key = new ResourceLocation("tardis", "skaro_hills");
-        } else if (fog.getSkyColor() == TARDISBiomeData.DESERT.getSkyColour()) {
-            key = new ResourceLocation("tardis", "skaro_desert");
-        } else if (fog.getSkyColor() == TARDISBiomeData.BADLANDS.getSkyColour()) {
-            key = new ResourceLocation("tardis", "gallifrey_badlands");
-        } else if (fog.getSkyColor() == TARDISBiomeData.ERODED.getSkyColour()) {
-            key = new ResourceLocation("tardis", "gallifrey_eroded");
-        } else if (fog.getSkyColor() == TARDISBiomeData.PLATEAU.getSkyColour()) {
-            key = new ResourceLocation("tardis", "gallifrey_plateau");
-        } else {
-            key = registry.getKey(base);
-        }
-        if (key != null) {
-            return key.toString();
-        } else {
-            switch (world.getEnvironment()) {
-                case NETHER -> {
-                    return "minecraft:nether_wastes";
-                }
-                case THE_END -> {
-                    return "minecraft:the_end";
-                }
-                default -> {
-                    if (world.getName().contains("skaro")) {
-                        return "tardis:skaro_lakes";
-                    } else if (world.getName().contains("gallifrey")) {
-                        return "tardis:gallifrey_badlands";
-                    } else {
-                        return "minecraft:plains";
-                    }
-                }
-            }
-        }
-    }
-
-    public static String getBiomeKey(Chunk c) {
-        LevelChunk chunk = ((CraftChunk) c).getHandle();
-        Biome base = chunk.getNoiseBiome(8, 64, 8);
-        BiomeSpecialEffects fog = base.getSpecialEffects();
-        if (fog.getSkyColor() == TARDISBiomeData.LAKES.getSkyColour()) {
-            return "tardis:skaro_lakes";
-        } else if (fog.getSkyColor() == TARDISBiomeData.HILLS.getSkyColour()) {
-            return "tardis:skaro_hills";
-        } else if (fog.getSkyColor() == TARDISBiomeData.DESERT.getSkyColour()) {
-            return "tardis:skaro_desert";
-        } else if (fog.getSkyColor() == TARDISBiomeData.BADLANDS.getSkyColour()) {
-            return "tardis:gallifrey_badlands";
-        } else if (fog.getSkyColor() == TARDISBiomeData.ERODED.getSkyColour()) {
-            return "tardis:gallifrey_eroded";
-        } else if (fog.getSkyColor() == TARDISBiomeData.PLATEAU.getSkyColour()) {
-            return "tardis:gallifrey_plateau";
-        } else {
-            Bukkit.getLogger().log(Level.INFO, "Biome key was not found for chunk skyColour");
-            DedicatedServer dedicatedServer = ((CraftServer) Bukkit.getServer()).getServer();
-            Registry<Biome> registry = dedicatedServer.registryAccess().registry(Registry.BIOME_REGISTRY).get();
-            return registry.getKey(base).toString();
-        }
     }
 }
--- src/main/java/me/eccentric_nz/tardischunkgenerator/custombiome/TARDISBiomeData.java
+++ src/main/java/me/eccentric_nz/tardischunkgenerator/custombiome/TARDISBiomeData.java
@@ -16,34 +16,6 @@ public class TARDISBiomeData {
             false
     );
 
-    public static final CustomBiomeData ERODED = new CustomBiomeData(
-            "eroded_badlands",
-            "gallifrey_eroded",
-            2.0f,
-            0,
-            16771522,
-            4184548,
-            340786,
-            16763001,
-            16309733,
-            16714324,
-            false
-    );
-
-    public static final CustomBiomeData PLATEAU = new CustomBiomeData(
-            "wooded_badlands",
-            "gallifrey_plateau",
-            2.0f,
-            0,
-            16771522,
-            4184548,
-            340786,
-            16763000,
-            16309733,
-            16714324,
-            false
-    );
-
     public static final CustomBiomeData DESERT = new CustomBiomeData(
             "desert",
             "skaro_desert",
@@ -53,34 +25,6 @@ public class TARDISBiomeData {
             4187262,
             340757,
             10648061,
-            0,
-            0,
-            false
-    );
-
-    public static final CustomBiomeData HILLS = new CustomBiomeData(
-            "desert",
-            "skaro_hills",
-            2.0f,
-            0,
-            13615871,
-            4187262,
-            340757,
-            10648062,
-            0,
-            0,
-            false
-    );
-
-    public static final CustomBiomeData LAKES = new CustomBiomeData(
-            "desert",
-            "skaro_lakes",
-            2.0f,
-            0,
-            13615871,
-            4187262,
-            340757,
-            10648063,
             0,
             0,
             false
